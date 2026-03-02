4be288ebbb5fe075908e0daa26eec72c3441e4ef
t00620168 <tujun6@huawei.com>
2026-03-02 17:01:43 +0800

    MoeInitRoutingV3算子性能优化实践与效果分析

---
+++ Samples/2_Performance/moe_init_routing/src/moe_common.h
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file moe_common.h
+ * \brief moe Example
+ */
+
+constexpr static int64_t BLOCK_BYTES = 32;
+constexpr static int64_t SIMT_THREAD_NUM = 2048;
+
+constexpr static int64_t MERGE_LIST_TWO = 2;
+constexpr static int64_t MERGE_LIST_THREE = 3;
+constexpr static int64_t MERGE_LIST_FOUR = 4;
+constexpr static int64_t MERGE_LIST_IDX_TWO = 2;
+constexpr static int64_t MERGE_LIST_IDX_THREE = 3;
+
+template <typename T>
+__aicore__ inline T Min(T a, T b)
+{
+    return a > b ? b : a;
+}
+
+template <typename T>
+__aicore__ inline T Max(T a, T b)
+{
+    return a < b ? b : a;
+}
+
+__aicore__ inline int64_t Align(int64_t elementNum, int64_t bytes)
+{
+    if (bytes == 0) {
+        return 0;
+    }
+    return (elementNum * bytes + BLOCK_BYTES - 1) / BLOCK_BYTES * BLOCK_BYTES / bytes;
+}
+
+__aicore__ inline int64_t AlignBytes(int64_t elementNum, int64_t bytes)
+{
+    return (elementNum * bytes + BLOCK_BYTES - 1) / BLOCK_BYTES * BLOCK_BYTES;
+}
