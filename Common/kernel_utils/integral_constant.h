d1f310a92a42659cda4a5a696ad80f2835ba0859
smdbha <panzhijie2@huawei.com>
2026-03-06 15:55:27 +0800

    modify according to the review comments

--- Common/kernel_utils/integral_constant.h
+++ Common/kernel_utils/integral_constant.h
@@ -34,22 +34,5 @@ template <int32_t t>
 using Int = AscendC::Std::integral_constant<int32_t, t>;
 
 using _0 = Int<0>;
-using _1 = Int<1>;
-using _2 = Int<2>;
-using _4 = Int<4>;
-using _8 = Int<8>;
-using _16 = Int<16>;
-using _32 = Int<32>;
-using _64 = Int<64>;
-using _128 = Int<128>;
-using _256 = Int<256>;
-using _512 = Int<512>;
-using _1024 = Int<1024>;
-using _2048 = Int<2048>;
-using _4096 = Int<4096>;
-using _8192 = Int<8192>;
-using _16384 = Int<16384>;
-using _32768 = Int<32768>;
-using _65536 = Int<65536>;
 
 #endif
