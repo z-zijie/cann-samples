f06fe2275704b53ea21c0feb6970fe4b54a4a07e
smdbha <panzhijie2@huawei.com>
2026-03-06 09:44:21 +0800

    adjust file directory

--- Common/kernel_utils/common_utils.h
+++ Common/kernel_utils/common_utils.h
@@ -71,22 +71,6 @@ struct MatmulShape {
     int64_t b;
 };
 
-__host_aicore__ inline int64_t CeilDiv(int64_t a, int64_t b)
-{
-    if (b == 0) {
-        return a;
-    }
-    return (a + b - 1) / b;
-}
-
-__host_aicore__ inline int64_t CeilAlign(int64_t a, int64_t b)
-{
-    if (b == 0) {
-        return a;
-    }
-    return (a + b - 1) / b * b;
-}
-
 template <typename T>
 __aicore__ inline T Max(T a, T b)
 {
@@ -107,6 +91,22 @@ __aicore__ inline uint64_t CeilDiv(uint64_t a, uint64_t b)
     return (a + b - 1) / b;
 }
 
+__host_aicore__ inline int64_t CeilDiv(int64_t a, int64_t b) 
+{ 
+    if (b == 0) { 
+        return a; 
+    } 
+    return (a + b - 1) / b; 
+} 
+
+
+__host_aicore__ inline int64_t CeilAlign(int64_t a, int64_t b) 
+{ 
+    if (b == 0) { 
+        return a; 
+    } 
+    return (a + b - 1) / b * b; 
+}
 __aicore__ inline uint64_t Align(uint64_t a, uint64_t b)
 {
     if (b == 0) {
