d860a412b68ef414ffcf6b19f371a0bb2cf153c2
Hu1L1 <chenshujian5@huawei.com>
2026-03-10 10:48:25 +0800

    [mxfp4 matmul stepbystep] demo0

---
+++ Samples/2_Performance/matmul_story/matmul_tutorials/0_naive/examples/matmul_tutorial_mxfp4_base.cpp
@@ -0,0 +1,165 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <memory>
+#include <random>
+
+#if ASC_DEVKIT_MAJOR >= 9
+#include "kernel_basic_intf.h"
+#else
+#include "kernel_operator.h"
+#endif
+#include "acl/acl.h"
+#include "tiling/platform/platform_ascendc.h"
+#include "../../common/host_utils/io_utils.h"
+#include "../include/kernel/quant_matmul_mx_kernel_base_impl.h"
+#include "../include/blcok/block_mmad_mx.h"
+#include "../include/blcok/block_scheduler_mx_base.h"
+#include "../include/utils/quant_matmul_constant.h"
+
+void printUsage(const std::string& programName)
+{
+    std::cerr << "Usage: " << programName << " m k n" << std::endl;
+    std::cerr << "Args: " << std::endl;
+    std::cerr << "  m: row of matrix A" << std::endl;
+    std::cerr << "  k: col of matrix A" << std::endl;
+    std::cerr << "  n: col of matrix B" << std::endl;
+    std::cerr << "Example: " << programName << " 100 50 200" << std::endl;
+}
+
+void parseArguments(int argc, char* argv[], int& m, int& k, int& n)
+{
+    if (argc >= 2 && (std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h")) {
+        printUsage(argv[0]);
+        exit(1);
+    }
+    if (argc < 4) {
+        throw std::invalid_argument("ERROR: Lacks Arguments");
+    }
+    try {
+        m = std::stoi(argv[1]);
+        k = std::stoi(argv[2]);
+        n = std::stoi(argv[3]);
+    } catch (const std::invalid_argument& e) {
+        throw std::invalid_argument("ERROR: m k n must be Integer");
+    }
+
+    if (m <= 0 || k <= 0 || n <= 0) {
+        throw std::invalid_argument("ERROR: m k n must be positive");
+    }
+
+    if (k % 2 != 0) {
+        throw std::invalid_argument("ERROR: k must be an even number");
+    }
+
+    if (CeilDiv(k, 32) % 2 != 0) {
+        throw std::invalid_argument("ERROR: k should satisfy that CeilDiv(k, 32) is an even number");
+    }
+}
+int main(int argc, char* argv[])
+{
+    int m, k, n;
+    try {
+        parseArguments(argc, argv, m, k, n);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        printUsage(argv[0]);
+        return 1;
+    }
+
+    // resource Init
+    int32_t deviceId = 0;
+    aclrtStream stream;
+    auto ret = aclInit(nullptr);
+    CHECK_COND(ret == ACL_SUCCESS, "aclInit failed.", return 1);
+    ret = aclrtSetDevice(deviceId);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtSetDevice failed.", return 1);
+    ret = aclrtCreateStream(&stream);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtCreateStream failed.", return 1);
+
+    // Host Input/Output construct
+    std::vector<uint8_t> hostA((m * k + 1) >> 1, 0);
+    std::vector<uint8_t> hostB((k * n + 1) >> 1, 0);
+    std::vector<uint8_t> hostScaleA(m * CeilDiv(k, MXFP_DIVISOR_SIZE) * MXFP_MULTI_BASE_SIZE, 0);
+    std::vector<uint8_t> hostScaleB(n * CeilDiv(k, MXFP_DIVISOR_SIZE) * MXFP_MULTI_BASE_SIZE, 0);
+    std::vector<float> hostOutput(m * n, 0);
+    std::vector<float> Golden(m * n, 0);
+    auto sizeA = static_cast<size_t>(1) * hostA.size() * sizeof(uint8_t);
+    auto sizeB = static_cast<size_t>(1) * hostB.size() * sizeof(uint8_t);
+    auto sizeScaleA = static_cast<size_t>(1) * hostScaleA.size() * sizeof(uint8_t);
+    auto sizeScaleB = static_cast<size_t>(1) * hostScaleB.size() * sizeof(uint8_t);
+    auto sizeOutput = static_cast<size_t>(1) * hostOutput.size() * sizeof(float);
+    ReadFile("./input/input_a.bin", sizeA, hostA.data(), sizeA);
+    ReadFile("./input/input_b.bin", sizeB, hostB.data(), sizeB);
+    ReadFile("./input/input_scaleA.bin", sizeScaleA, hostScaleA.data(), sizeScaleA);
+    ReadFile("./input/input_scaleB.bin", sizeScaleB, hostScaleB.data(), sizeScaleB);
+    ReadFile("./output/golden_out.bin", sizeOutput, Golden.data(), sizeOutput);
+
+    // Device Addredd malloc
+    GM_ADDR deviceA = nullptr;
+    GM_ADDR deviceB = nullptr;
+    GM_ADDR deviceScaleA = nullptr;
+    GM_ADDR deviceScaleB = nullptr;
+    GM_ADDR deviceOutput = nullptr;
+    ret = aclrtMalloc((void**)&deviceA, sizeA, ACL_MEM_MALLOC_HUGE_FIRST);
+    std::unique_ptr<void, aclError (*)(void*)> DeviceAAddr(deviceA, aclrtFree);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMalloc deviceA failed.", return 1);
+    ret = aclrtMalloc((void**)&deviceB, sizeB, ACL_MEM_MALLOC_HUGE_FIRST);
+    std::unique_ptr<void, aclError (*)(void*)> DeviceBAddr(deviceB, aclrtFree);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMalloc deviceB failed.", return 1);
+    ret = aclrtMalloc((void**)&deviceScaleA, sizeScaleA, ACL_MEM_MALLOC_HUGE_FIRST);
+    std::unique_ptr<void, aclError (*)(void*)> DeviceScaleAAddr(deviceScaleA, aclrtFree);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMalloc deviceScaleA failed.", return 1);
+    ret = aclrtMalloc((void**)&deviceScaleB, sizeScaleB, ACL_MEM_MALLOC_HUGE_FIRST);
+    std::unique_ptr<void, aclError (*)(void*)> DeviceScaleBAddr(deviceScaleB, aclrtFree);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMalloc deviceScaleB failed.", return 1);
+    ret = aclrtMalloc((void**)&deviceOutput, sizeOutput, ACL_MEM_MALLOC_HUGE_FIRST);
+    std::unique_ptr<void, aclError (*)(void*)> DeviceOutputAddr(deviceOutput, aclrtFree);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMalloc deviceOutput failed.", return 1);
+
+    // memcpy from Host to Device
+    ret = aclrtMemcpy(deviceA, sizeA, hostA.data(), sizeA, ACL_MEMCPY_HOST_TO_DEVICE);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMemcpy deviceA failed.", return 1);
+    ret = aclrtMemcpy(deviceB, sizeB, hostB.data(), sizeB, ACL_MEMCPY_HOST_TO_DEVICE);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMemcpy deviceB failed.", return 1);
+    ret = aclrtMemcpy(deviceScaleA, sizeScaleA, hostScaleA.data(), sizeScaleA, ACL_MEMCPY_HOST_TO_DEVICE);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMemcpy deviceScaleA failed.", return 1);
+    ret = aclrtMemcpy(deviceScaleB, sizeScaleB, hostScaleB.data(), sizeScaleB, ACL_MEMCPY_HOST_TO_DEVICE);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMemcpy deviceScaleB failed.", return 1);
+
+    auto ascendcPlatform = platform_ascendc::PlatformAscendCManager::GetInstance();
+    CHECK_COND(ascendcPlatform != nullptr, "get ascendcPlatform failed.", return 1);
+    uint32_t blockDim = ascendcPlatform->GetCoreNumAic();
+    Kernel::QuantMatmulMxfp4BaseKernel<<<blockDim, nullptr, stream>>>(m, k, n, deviceA, deviceB, deviceScaleA, deviceScaleB, deviceOutput);
+    
+    // Synchronize
+    ret = aclrtSynchronizeStream(stream);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtSynchronizeStream failed.", return 1);
+
+    // memcpy from Device to Host
+    ret = aclrtMemcpy(hostOutput.data(), sizeOutput, deviceOutput, sizeOutput, ACL_MEMCPY_DEVICE_TO_HOST);
+    CHECK_COND(ret == ACL_SUCCESS, "aclrtMemcpy deviceOutput failed.", return 1);
+
+    WriteFile("./output/npu_out.bin", hostOutput.data(), sizeOutput);
+    aclrtDestroyStream(stream);
+    aclrtResetDevice(deviceId);
+    aclFinalize();
+    for (int i = 0; i < m * n; ++i) {
+        if (abs(Golden[i] - hostOutput[i]) >= 0.0001) {
+            std::cout << "Compare Index " << i << " Failed." << std::endl;
+            std::cout << "Golden[" << i << "]: " << Golden[i] << " ; HostOutput[" << i << " ]" << hostOutput[i] << std::endl;
+        }
+    }
+    std::cout << "Test Pass!" << std::endl;
+    return 0;
+}
