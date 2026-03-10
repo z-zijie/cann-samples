11cb39d79c941dff2bea39434d9245056346693f
Hu1L1 <chenshujian5@huawei.com>
2026-03-10 10:48:25 +0800

    [mxfp4 matmul stepbystep] demo0

---
+++ Samples/2_Performance/matmul_story/matmul_tutorials/common/host_utils/common_utils.h
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file common_utils.h
+ * \brief
+ */
+
+#ifndef COMMON_UTILS_H
+#define COMMON_UTILS_H
+#include <iostream>
+#include <fstream>
+
+#define ERROR_LOG(fmt, args...) fprintf(stdout, "[ERROR]  " fmt "\n", ##args)
+
+// 两参：条件不满足时打印错误并 return 1
+#define CHECK_COND(cond, message)                                                                               \
+    do {                                                                                                       \
+        if (!(cond)) {                                                                                         \
+            std::cerr << "ERROR: " << (message) << std::endl;                                                  \
+            return 1;                                                                                          \
+        }                                                                                                      \
+    } while (0)
+    
+template <typename T>
+static T CeilDiv(T num1, T num2)
+{
+    return (num1 + num2 - 1) / num2;
+}
+
+template <typename T>
+static T CeilAlign(T num1, T num2)
+{
+    return CeilDiv(num1, num2) * num2;
+}
+
+template <typename T>
+static T FloorAlign(T num1, T num2)
+{
+    return num1 / num2 * num2;
+}
+
+#endif // COMMON_UTILS_H
