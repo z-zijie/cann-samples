6a4941b9c24d2a45c9206dd5020cc0727fbba5e4
cardiac_index <wangjianhao12@huawei.com>
2026-03-11 20:15:24 +0800

    modify gendata and verify

--- Samples/2_Performance/full_quant_fused_infer_attention_score_story/common/golden/gen_data.py
+++ Samples/2_Performance/full_quant_fused_infer_attention_score_story/common/golden/gen_data.py
@@ -26,15 +26,15 @@ def gen_golden_data_simple():
     s2 = 8192
     d = 128
 
-    inputLayout = 'BNSD'
+    input_layout = 'BNSD'
     scale_value = 0.088388
 
     q_tensor = torch.randint(-127, 127, (b, n1, s1, d), dtype=torch.int8)
     k_tensor = torch.randint(-127, 127, (b, n2, s2, d), dtype=torch.int8)
     v_tensor = torch.randint(-127, 127, (b, n2, s2, d), dtype=torch.int8)
-    keyAntiquantScale = torch.rand((1, 1, 32, 1), dtype=torch.float32)
-    valueAntiquantScale = torch.rand((1, 1, 32, 1), dtype=torch.float32)
-    dequantScaleQuery = torch.rand((1, 1, 64, 1), dtype=torch.float32)
+    key_antiquant_scale = torch.rand((1, 1, 32, 1), dtype=torch.float32)
+    value_antiquant_scale = torch.rand((1, 1, 32, 1), dtype=torch.float32)
+    dequant_scale_query = torch.rand((1, 1, 64, 1), dtype=torch.float32)
 
     q_tensor_tmp = q_tensor.numpy()
     k_tensor_tmp = k_tensor.numpy()
@@ -43,27 +43,29 @@ def gen_golden_data_simple():
     q_tensor_tmp.tofile("./input/input_0.bin")
     k_tensor_tmp.tofile("./input/input_1.bin")
     v_tensor_tmp.tofile("./input/input_2.bin")
-    keyAntiquantScale.numpy().tofile("./input/input_15.bin")
-    valueAntiquantScale.numpy().tofile("./input/input_17.bin")
-    dequantScaleQuery.numpy().tofile("./input/input_27.bin")
+    key_antiquant_scale.numpy().tofile("./input/input_15.bin")
+    value_antiquant_scale.numpy().tofile("./input/input_17.bin")
+    dequant_scale_query.numpy().tofile("./input/input_27.bin")
 
     q_tensor = q_tensor.npu()
     k_tensor = k_tensor.npu()
     v_tensor = v_tensor.npu()
-    keyAntiquantScale = keyAntiquantScale.npu()
-    valueAntiquantScale = valueAntiquantScale.npu()
-    dequantScaleQuery = dequantScaleQuery.npu()
+    key_antiquant_scale = key_antiquant_scale.npu()
+    value_antiquant_scale = value_antiquant_scale.npu()
+    dequant_scale_query = dequant_scale_query.npu()
 
-    npu_out = torch.ops.npu.npu_fused_infer_attention_score_v2(q_tensor, k_tensor, v_tensor, dequant_scale_key = keyAntiquantScale, dequant_scale_value = valueAntiquantScale,
-                                                    dequant_scale_query = dequantScaleQuery, num_query_heads = n1,
-                                                    softmax_scale = scale_value, input_layout = inputLayout,
-                                                    num_key_value_heads = n2, query_quant_mode = 7,
-                                                    key_quant_mode = 7,
-                                                    value_quant_mode = 7, inner_precise = 0, return_softmax_lse = 0,
-                                                    query_dtype = torch_npu.float8_e4m3fn, key_dtype = torch_npu.float8_e4m3fn, value_dtype = torch_npu.float8_e4m3fn)
+    npu_out = torch.ops.npu.npu_fused_infer_attention_score_v2(q_tensor, k_tensor, v_tensor,
+                                                            dequant_scale_key=key_antiquant_scale,
+                                                            dequant_scale_value=value_antiquant_scale,
+                                                            dequant_scale_query=dequant_scale_query, num_query_heads=n1,
+                                                            softmax_scale=scale_value, input_layout=input_layout,
+                                                            num_key_value_heads=n2, query_quant_mode=7,
+                                                            key_quant_mode=7, value_quant_mode=7,
+                                                            inner_precise=0, return_softmax_lse=0,
+                                                            query_dtype=torch_npu.float8_e4m3fn,
+                                                            key_dtype=torch_npu.float8_e4m3fn,
+                                                            value_dtype=torch_npu.float8_e4m3fn)
     npu_out[0].cpu().numpy().tofile("./output/golden_out.bin")
-    print("q_tensor dtype")
-    print(q_tensor.dtype)
 
 
 if __name__ == "__main__":
