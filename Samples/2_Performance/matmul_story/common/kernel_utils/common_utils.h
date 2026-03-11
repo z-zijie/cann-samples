3b7e081770f5788fceed11c026ce92b653914e9c
smdbha <panzhijie2@huawei.com>
2026-03-11 19:56:37 +0800

    add performance mxfp4 tiling

--- Samples/2_Performance/matmul_story/common/kernel_utils/common_utils.h
+++ Samples/2_Performance/matmul_story/common/kernel_utils/common_utils.h
@@ -39,13 +39,6 @@ constexpr int MNK_B = 3;
 constexpr int MNK_M0 = 4;
 constexpr int MNK_N0 = 5;
 
-struct MatmulShape {
-    int64_t m;
-    int64_t n;
-    int64_t k;
-    int64_t b;
-};
-
 template <typename T>
 __aicore__ inline T Max(T a, T b)
 {
@@ -66,7 +59,7 @@ __aicore__ inline uint64_t CeilDiv(uint64_t a, uint64_t b)
     return (a + b - 1) / b;
 }
 
-__host_aicore__ inline int64_t CeilDiv(int64_t a, int64_t b) 
+__aicore__ inline int64_t CeilDiv(int64_t a, int64_t b) 
 { 
     if (b == 0) { 
         return a; 
@@ -75,13 +68,14 @@ __host_aicore__ inline int64_t CeilDiv(int64_t a, int64_t b)
 } 
 
 
-__host_aicore__ inline int64_t CeilAlign(int64_t a, int64_t b) 
+__aicore__ inline int64_t CeilAlign(int64_t a, int64_t b) 
 { 
     if (b == 0) { 
         return a; 
     } 
     return (a + b - 1) / b * b; 
 }
+
 __aicore__ inline uint64_t Align(uint64_t a, uint64_t b)
 {
     if (b == 0) {
