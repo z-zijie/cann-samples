63fcb95be20085702b77ea3addcc44d027a4e494
wangyunxiang1 <wangyunxiang6@h-partners.com>
2026-09-23 08:45:06 +0800

    update cann-samples gcc15
    Co-authored-by: wangyunxiang<wangyunxiang6@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !444 merge master into master
    
    update cann-samples gcc15
    
    Created-by: wangyunxiang1
    Commit-by: wangyunxiang
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/cann-samples!444

--- .gitcode/scripts/compile.sh
+++ .gitcode/scripts/compile.sh
@@ -14,12 +14,36 @@ REPOSITORY_NAME="ops-ras"
 echo $(grep -E "^VERSION_ID=" /etc/os-release | cut -d'"' -f2)
 export PATH=/opt/buildtools/python-3.10.2/bin:$PATH
 if [[ "${task_name}" == *ubuntu24* ]]; then
-    sudo update-alternatives --set gcc /usr/bin/gcc-14
+    if sudo update-alternatives --set gcc /usr/bin/gcc-16 2>/dev/null; then
+        echo "Switched to gcc-16"
+    elif sudo update-alternatives --set gcc /usr/bin/gcc-15 2>/dev/null; then
+        echo "Switched to gcc-15"
+    elif sudo update-alternatives --set gcc /usr/bin/gcc-14 2>/dev/null; then
+        echo "gcc-16/15 not available, fell back to gcc-14"
+    fi
 else
     if [[ -f "/opt/rh/devtoolset-7/enable" ]]; then
         echo "source devtoolset"
         source /opt/rh/devtoolset-7/enable
     fi
+fi
+if gcc --version | head -n1 | grep -q "15\."; then
+    rm -rf /home/jenkins/opensource/lib_cache
+    if [ -d /home/jenkins/opensource/gcc15 ]; then
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15/lib_cache/ /home/jenkins/opensource/lib_cache
+    elif [ -d /home/jenkins/opensource/gcc15x86 ]; then
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/abseil-cpp
+        rm -rf /home/jenkins/opensource/gcc15x86/lib_cache/device/abseil-cpp
+        ln -s /home/jenkins/opensource/gcc15x86/lib_cache/ /home/jenkins/opensource/lib_cache
+    fi
+elif gcc --version | head -n1 | grep -q "14\."; then
+    gcc --version
+else
+    gcc --version
+    rm -rf /home/jenkins/opensource/lib_cache
+    ln -s /home/jenkins/opensource/ubuntu20/lib_cache /home/jenkins/opensource/lib_cache
 fi
 gcc --version
 
